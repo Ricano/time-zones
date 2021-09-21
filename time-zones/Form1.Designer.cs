342018fbc2e5936624053b82c7f877b5cc9f907b
Ricano <ricardofilipecaetano@gmail.com>
2021-09-21 10:24:02 +0100

    added labels

--- time-zones/Form1.Designer.cs
+++ time-zones/Form1.Designer.cs
@@ -29,13 +29,152 @@ namespace time_zones
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelPortugal = new System.Windows.Forms.Label();
+            this.labelIndia = new System.Windows.Forms.Label();
+            this.labelPrimeiro = new System.Windows.Forms.Label();
+            this.labelSegundo = new System.Windows.Forms.Label();
+            this.labelTerceiro = new System.Windows.Forms.Label();
+            this.portugal = new System.Windows.Forms.Label();
+            this.india = new System.Windows.Forms.Label();
+            this.primeiro = new System.Windows.Forms.Label();
+            this.segundo = new System.Windows.Forms.Label();
+            this.terceiro = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelPortugal
+            // 
+            this.labelPortugal.AutoSize = true;
+            this.labelPortugal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPortugal.Location = new System.Drawing.Point(12, 10);
+            this.labelPortugal.Name = "labelPortugal";
+            this.labelPortugal.Size = new System.Drawing.Size(84, 24);
+            this.labelPortugal.TabIndex = 0;
+            this.labelPortugal.Text = "Portugal:";
+            // 
+            // labelIndia
+            // 
+            this.labelIndia.AutoSize = true;
+            this.labelIndia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIndia.Location = new System.Drawing.Point(12, 50);
+            this.labelIndia.Name = "labelIndia";
+            this.labelIndia.Size = new System.Drawing.Size(55, 24);
+            this.labelIndia.TabIndex = 1;
+            this.labelIndia.Text = "Índia:";
+            // 
+            // labelPrimeiro
+            // 
+            this.labelPrimeiro.AutoSize = true;
+            this.labelPrimeiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrimeiro.Location = new System.Drawing.Point(1, 100);
+            this.labelPrimeiro.Name = "labelPrimeiro";
+            this.labelPrimeiro.Size = new System.Drawing.Size(102, 20);
+            this.labelPrimeiro.TabIndex = 2;
+            this.labelPrimeiro.Text = "+ 0 HORAS";
+            // 
+            // labelSegundo
+            // 
+            this.labelSegundo.AutoSize = true;
+            this.labelSegundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSegundo.Location = new System.Drawing.Point(1, 140);
+            this.labelSegundo.Name = "labelSegundo";
+            this.labelSegundo.Size = new System.Drawing.Size(102, 20);
+            this.labelSegundo.TabIndex = 3;
+            this.labelSegundo.Text = "+ 0 HORAS";
+            // 
+            // labelTerceiro
+            // 
+            this.labelTerceiro.AutoSize = true;
+            this.labelTerceiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTerceiro.Location = new System.Drawing.Point(1, 180);
+            this.labelTerceiro.Name = "labelTerceiro";
+            this.labelTerceiro.Size = new System.Drawing.Size(102, 20);
+            this.labelTerceiro.TabIndex = 4;
+            this.labelTerceiro.Text = "+ 0 HORAS";
+            // 
+            // portugal
+            // 
+            this.portugal.AutoSize = true;
+            this.portugal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portugal.Location = new System.Drawing.Point(134, 9);
+            this.portugal.Name = "portugal";
+            this.portugal.Size = new System.Drawing.Size(80, 24);
+            this.portugal.TabIndex = 5;
+            this.portugal.Text = "00:00:00";
+            // 
+            // india
+            // 
+            this.india.AutoSize = true;
+            this.india.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.india.Location = new System.Drawing.Point(134, 50);
+            this.india.Name = "india";
+            this.india.Size = new System.Drawing.Size(55, 24);
+            this.india.TabIndex = 6;
+            this.india.Text = "00:00";
+            // 
+            // primeiro
+            // 
+            this.primeiro.AutoSize = true;
+            this.primeiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.primeiro.Location = new System.Drawing.Point(134, 96);
+            this.primeiro.Name = "primeiro";
+            this.primeiro.Size = new System.Drawing.Size(55, 24);
+            this.primeiro.TabIndex = 7;
+            this.primeiro.Text = "00:00";
+            // 
+            // segundo
+            // 
+            this.segundo.AutoSize = true;
+            this.segundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.segundo.Location = new System.Drawing.Point(134, 136);
+            this.segundo.Name = "segundo";
+            this.segundo.Size = new System.Drawing.Size(55, 24);
+            this.segundo.TabIndex = 8;
+            this.segundo.Text = "00:00";
+            // 
+            // terceiro
+            // 
+            this.terceiro.AutoSize = true;
+            this.terceiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.terceiro.Location = new System.Drawing.Point(134, 176);
+            this.terceiro.Name = "terceiro";
+            this.terceiro.Size = new System.Drawing.Size(55, 24);
+            this.terceiro.TabIndex = 9;
+            this.terceiro.Text = "00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.terceiro);
+            this.Controls.Add(this.segundo);
+            this.Controls.Add(this.primeiro);
+            this.Controls.Add(this.india);
+            this.Controls.Add(this.portugal);
+            this.Controls.Add(this.labelTerceiro);
+            this.Controls.Add(this.labelSegundo);
+            this.Controls.Add(this.labelPrimeiro);
+            this.Controls.Add(this.labelIndia);
+            this.Controls.Add(this.labelPortugal);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelPortugal;
+        private System.Windows.Forms.Label labelIndia;
+        private System.Windows.Forms.Label labelPrimeiro;
+        private System.Windows.Forms.Label labelSegundo;
+        private System.Windows.Forms.Label labelTerceiro;
+        private System.Windows.Forms.Label portugal;
+        private System.Windows.Forms.Label india;
+        private System.Windows.Forms.Label primeiro;
+        private System.Windows.Forms.Label segundo;
+        private System.Windows.Forms.Label terceiro;
     }
 }
 
--- time-zones/Form1.cs
+++ time-zones/Form1.cs
@@ -16,5 +16,7 @@ namespace time_zones
         {
             InitializeComponent();
         }
+
+       
     }
 }
